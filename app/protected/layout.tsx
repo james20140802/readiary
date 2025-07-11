ce1993fff82fc02ca06da1532f39ff9a9e50c084
SangChu04 <drchasekim@gmail.com>
2025-07-11 20:10:51 +0900

    :lipstick: Add padding for preventing navbar for screening content

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -2,9 +2,11 @@ import Navbar from '@/components/Navbar';
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      <main className="flex-1 pb-[64px] px-4 max-w-screen-md w-full mx-auto">{children}</main>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
+      <main className="flex-1 max-w-screen-md w-full mx-auto px-4 pt-4 md:pt-[56px] pb-20 md:pb-[64px]">
+        {children}
+      </main>
     </div>
   );
 }
