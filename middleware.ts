de9628718919e965e42caccc92a2ab90944b139f
SangChu04 <drchasekim@gmail.com>
2025-07-12 00:57:17 +0900

    :sparkles: Add redirection based on auth

--- middleware.ts
+++ middleware.ts
@@ -51,6 +51,17 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  if (
+    user &&
+    (request.nextUrl.pathname === '/' ||
+      request.nextUrl.pathname.startsWith('/login') ||
+      request.nextUrl.pathname.startsWith('/signup'))
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/protected/dashboard';
+    return NextResponse.redirect(url);
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
@@ -68,5 +79,5 @@ export async function updateSession(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/protected/:path*'],
+  matcher: ['/', '/login', '/signup', '/protected/:path*'],
 };
