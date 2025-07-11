2c107382fc60916a9aeeebf6195ac8dfc458b3ab
SangChu04 <drchasekim@gmail.com>
2025-07-12 00:10:01 +0900

    :label: Declare type used in profile page

---
+++ types/profile.ts
@@ -0,0 +1,9 @@
+export type Profile = {
+  id: string;
+  name: string;
+  nickname: string;
+  tag: string;
+  bio: string | null;
+  created_at: string | null;
+  profile_image: string | null;
+};
