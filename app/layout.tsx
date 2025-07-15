a0dd598e7e9c83c43469b8c5a5e9b8e11faacb4b
SangChu04 <drchasekim@gmail.com>
2025-07-15 12:06:30 +0900

    :sparkles: Add toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Toaster } from 'sonner';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,6 +31,7 @@ export default function RootLayout({
         <main className="max-w-screen-md mx-auto w-full px-3 sm:px-4 md:px-6 py-6 sm:py-10">
           {children}
         </main>
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   );
