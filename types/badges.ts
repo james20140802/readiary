50bc0c645d1b527a98708a273f442082d2b6e5df
SangChu04 <drchasekim@gmail.com>
2025-07-15 12:32:26 +0900

    :label: Add BadgeConditionDefinition type

--- types/badges.ts
+++ types/badges.ts
@@ -2,3 +2,9 @@ export type BadgeCheckResult = {
   badgeId: string;
   conditionMet: boolean;
 };
+
+export type BadgeConditionDefinition = {
+  code: string; // badgeId와 동일
+  description: string;
+  check: (userId: string) => Promise<boolean>;
+};
