c49052283edc624f8e2915a9f46e752da27f5760
SangChu04 <drchasekim@gmail.com>
2025-07-16 15:45:31 +0900

    :sparkles: Add header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'sonner';
+import Header from '@/components/Header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} overflow-x-hidden bg-white dark:bg-gray-900`}
       >
-        <main className="max-w-screen-md mx-auto w-full px-3 sm:px-4 md:px-6 py-6 sm:py-10">
+        <Header />
+        <main className="max-w-screen-md mx-auto w-full px-3 sm:px-4 md:px-6 py-6 sm:py-10 pt-[72px]">
           {children}
         </main>
         <Toaster richColors position="top-center" />
