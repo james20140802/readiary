100f4f073cea0c2328c517bca3470cc2ed7c7340
SangChu04 <drchasekim@gmail.com>
2025-07-18 00:58:56 +0900

    :label: Add types related to entry

---
+++ types/entry.ts
@@ -0,0 +1,31 @@
+import { Book } from './book';
+import { Profile } from './profile';
+
+export type Entry = {
+  id: string;
+  date: string;
+  summary: string | null;
+  from_page: number | null;
+  to_page: number | null;
+  is_private: boolean;
+  book: Book;
+};
+
+export type SocialFeedEntry = {
+  profile: Profile;
+  entry: Entry;
+};
+
+export type RawEntry = {
+  id: string;
+  summary: string | null;
+  date: string;
+  from_page: number | null;
+  to_page: number | null;
+  created_at: string | null;
+  user_books: {
+    user_id: string;
+    book_id: string;
+    books: Book;
+  };
+};
