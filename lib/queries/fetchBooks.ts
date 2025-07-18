9d923fd68ea6c0003ad8fdaabebcb59cfa0bb31f
SangChu04 <drchasekim@gmail.com>
2025-07-18 11:01:28 +0900

    :sparkles: Separate book fetching function

---
+++ lib/queries/fetchBooks.ts
@@ -0,0 +1,24 @@
+import { MyBook } from '@/types/book';
+import { createSupabaseServerClient } from '../supabase/server';
+
+export async function fetchMyBooksData(): Promise<MyBook[] | null> {
+  const supabase = await createSupabaseServerClient();
+
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (!user || userError) return null;
+
+  const { data, error } = await supabase
+    .from('user_books')
+    .select(
+      'id, progress, book_id, started_at, last_read_page, is_finished, books(title, author, total_pages)'
+    )
+    .eq('user_id', user.id);
+
+  if (!data || error) return [];
+
+  return data;
+}
