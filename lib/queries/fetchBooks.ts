05d492364964e471e05acf915f32b3bb5baf50a1
SangChu04 <drchasekim@gmail.com>
2025-07-20 00:51:10 +0900

    :alien: Add id element when fetching my book data

--- lib/queries/fetchBooks.ts
+++ lib/queries/fetchBooks.ts
@@ -14,7 +14,7 @@ export async function fetchMyBooksData(): Promise<MyBook[] | null> {
   const { data, error } = await supabase
     .from('user_books')
     .select(
-      'id, progress, book_id, started_at, last_read_page, is_finished, books(title, author, total_pages)'
+      'id, progress, book_id, started_at, last_read_page, is_finished, books(id, title, author, total_pages)'
     )
     .eq('user_id', user.id);
 
