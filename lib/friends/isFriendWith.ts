70c2a46b4773724e2dc04ed9c424947cd27ea6b0
SangChu04 <drchasekim@gmail.com>
2025-07-21 02:46:39 +0900

    :recycle: Refactor isFriendWith to get nickname and tag as parameter

--- lib/friends/isFriendWith.ts
+++ lib/friends/isFriendWith.ts
@@ -1,13 +1,34 @@
 import { createSupabaseServerClient } from '@/lib/supabase/server';
 
-export async function isFriendWith(userId1: string, userId2: string) {
+export async function isFriendWith({ nickname, tag }: { nickname: string; tag: string }) {
   const supabase = await createSupabaseServerClient();
+
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (!user || userError) return false;
+
+  const currentUserId = user.id;
+
+  // 상대 유저 ID 가져오기
+  const { data: profile, error: profileError } = await supabase
+    .from('profiles')
+    .select('id')
+    .eq('nickname', nickname)
+    .eq('tag', tag)
+    .maybeSingle();
+
+  if (!profile || profileError) return false;
+
+  const targetUserId = profile.id;
 
   const { data, error } = await supabase
     .from('friends')
     .select('id')
     .or(
-      `and(sender_id.eq.${userId1},recipient_id.eq.${userId2},accepted.eq.true),and(sender_id.eq.${userId2},recipient_id.eq.${userId1},accepted.eq.true)`
+      `and(sender_id.eq.${currentUserId},recipient_id.eq.${targetUserId},accepted.eq.true),and(sender_id.eq.${targetUserId},recipient_id.eq.${currentUserId},accepted.eq.true)`
     )
     .maybeSingle();
 
