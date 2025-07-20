b65b6ee22f1e2a485321b078a18c03a0c140ba7f
SangChu04 <drchasekim@gmail.com>
2025-07-21 02:49:12 +0900

    :sparkles: Add friend book detail page

---
+++ app/protected/social/[nicknameAndTag]/books/[bookId]/page.tsx
@@ -0,0 +1,32 @@
+import { fetchFriendBookEntries } from '@/lib/friends/fetchFriendBookEntries';
+import { notFound } from 'next/navigation';
+import BookDetailContent from './_components/BookDetailContent';
+import { isFriendWith } from '@/lib/friends/isFriendWith';
+
+interface FriendBookDetailPageProps {
+  params: Promise<{
+    nicknameAndTag: string;
+    bookId: string;
+  }>;
+}
+
+export default async function FriendBookDetailPage({ params }: FriendBookDetailPageProps) {
+  const { nicknameAndTag, bookId } = await params;
+  const [nickname, tag] = nicknameAndTag.split('-');
+
+  const isFriend = await isFriendWith({ nickname, tag });
+  console.log(isFriend);
+  if (!isFriend) {
+    return notFound();
+  }
+
+  const data = await fetchFriendBookEntries({ nickname, tag, bookId });
+
+  if (!data) {
+    return notFound();
+  }
+
+  return (
+    <BookDetailContent userBook={data.book} entries={data.entries} friendProfile={data.profile} />
+  );
+}
