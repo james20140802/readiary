1f053ce8b1c44f767142e45176171dde7574cade
SangChu04 <drchasekim@gmail.com>
2025-07-21 02:37:40 +0900

    :sparkles: Add href props in entry card

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -6,12 +6,13 @@ interface EntryCardProps {
   id: string;
   summary: string;
   date: string;
+  href?: string;
 }
 
-export default function EntryCard({ id, summary, date }: EntryCardProps) {
+export default function EntryCard({ id, summary, date, href }: EntryCardProps) {
   return (
     <Link
-      href={`/protected/entry/${id}`}
+      href={href ?? `/protected/entry/${id}`}
       className="block bg-gray-100 dark:bg-gray-800 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 hover:shadow-md transition cursor-pointer px-4 py-3 space-y-2"
     >
       <p className="text-base text-gray-800 dark:text-gray-100">{summary}</p>
