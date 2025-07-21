1d0c5ad3183ed9de9caa067980db2bf4c2b2b685
SangChu04 <drchasekim@gmail.com>
2025-07-21 23:15:30 +0900

    :lipstick: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './lib/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tint: '#3B82F6',
+        label: '#111827',
+        secondary: '#6B7280',
+        background: '#FFFFFF',
+        darkbg: '#1F2937',
+      },
+      fontSize: {
+        'page-title': ['1.5rem', '2rem'], // text-2xl leading-tight
+        'section-title': ['1.25rem', '1.75rem'], // text-xl leading-snug
+        'body-text': ['1rem', '1.5rem'], // text-base leading-relaxed
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
 };
