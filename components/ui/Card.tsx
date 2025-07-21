37b4e7343bfb59b70408dc4a0508a94957787e3e
SangChu04 <drchasekim@gmail.com>
2025-07-22 02:35:06 +0900

    :sparkles: Add onClick event on Card component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -5,11 +5,13 @@ interface CardProps {
   children: ReactNode;
   className?: string;
   hoverable?: boolean;
+  onClick?: () => void;
 }
 
-export default function Card({ children, className, hoverable = true }: CardProps) {
+export default function Card({ children, className, hoverable = true, onClick }: CardProps) {
   return (
     <div
+      onClick={onClick}
       className={clsx(
         'bg-background dark:bg-darkbg rounded-xl shadow-sm border border-gray-200 dark:border-gray-700 p-4',
         hoverable && 'hover:bg-gray-50 dark:hover:bg-darkbg/80',
