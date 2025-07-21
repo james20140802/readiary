2c1fde624e1cc0fde316fb0395333f7eb9a61061
SangChu04 <drchasekim@gmail.com>
2025-07-21 16:03:44 +0900

    :wrench: Add kakao domain to images url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['search1.kakaocdn.net', 't1.daumcdn.net'],
+  },
 };
 
 export default nextConfig;
