60f1192da91a12fefd1e82d00894986dfbc0e15d
SangChu04 <drchasekim@gmail.com>
2026-02-08 20:58:14 +0900

    :arrow_up: Change next config w.r.t. next version

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,19 @@ import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['search1.kakaocdn.net', 't1.daumcdn.net'],
+    // 기존 domains 대신 remotePatterns 사용 (보안 강화)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'search1.kakaocdn.net',
+        pathname: '/**', // 해당 도메인의 모든 경로 허용
+      },
+      {
+        protocol: 'https',
+        hostname: 't1.daumcdn.net',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
